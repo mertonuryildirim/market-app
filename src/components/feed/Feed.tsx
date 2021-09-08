6d2ae76cf8e3fc26e0e9e243b9b6aa72b4d30d52
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-09-08 15:58:31 +0300

    card styles added & sample card added into feed

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './Feed.css';
+import ItemTypes from './ItemTypes';
 
 const Feed = () => {
     return (
         <div className="feed">
-            <h1>Feed</h1>
+            <h2>Products</h2>
+
+            <ItemTypes />
+
+            <div className="card">
+                <div className="container">
+                    <h4>
+                        <b>Card Header</b>
+                    </h4>
+                    <p>Card Content</p>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/feed/ItemTypes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ItemTypes.css';
+
+const ItemTypes = () => {
+    return (
+        <div className="item-types">
+            <button>mug</button>
+            <button>shirt</button>
+        </div>
+    );
+};
+
+export default ItemTypes;
