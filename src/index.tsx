6aeb5add5a195236a4ba33c6278ee727cef0fec2
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-09-09 14:24:37 +0300

    create redux store & implemented

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
 
 import App from './App';
 import './index.css';
+import rootReducer from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//apply middleware içerisine saga koy.
+const store = createStore(rootReducer, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
