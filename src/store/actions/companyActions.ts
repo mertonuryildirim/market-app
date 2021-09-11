ec24ab3dedd59f6f98fef3c73192807e4ac32573
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-09-11 03:48:53 +0300

    changed companies action types

--- src/store/actions/companyActions.ts
+++ src/store/actions/companyActions.ts
@@ -1,15 +1,23 @@
-import { CompanyAction, Company } from '../../types/company';
+import {
+    CompanyAction,
+    Company,
+    GET_COMPANIES,
+    GET_COMPANIES_SUCCESS,
+    GET_COMPANIES_ERROR,
+} from '../../types/company';
 import { companyActionTypes } from './actionTypes';
 
-export const getCompanies = (): CompanyAction => ({
+export const getCompanies = (): GET_COMPANIES => ({
     type: companyActionTypes.GET_COMPANIES,
 });
 
-export const getCompaniesSuccess = (companies: Company[]): CompanyAction => ({
+export const getCompaniesSuccess = (
+    companies: Company[],
+): GET_COMPANIES_SUCCESS => ({
     type: companyActionTypes.GET_COMPANIES_SUCCESS,
     payload: companies,
 });
 
-export const getCompaniesError = (): CompanyAction => ({
+export const getCompaniesError = (): GET_COMPANIES_ERROR => ({
     type: companyActionTypes.GET_COMPANIES_ERROR,
 });
