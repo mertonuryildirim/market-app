2a621056c3e5652d6f9306a711bef8418022eccc
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-09-11 03:14:33 +0300

    changed item actions types

--- src/store/actions/itemActions.ts
+++ src/store/actions/itemActions.ts
@@ -1,16 +1,22 @@
-import { ItemAction, Item, FilteringData } from '../../types/item';
+import {
+    Item,
+    FilteringData,
+    GET_ITEMS,
+    GET_ITEMS_ERROR,
+    GET_ITEMS_SUCCESS,
+} from '../../types/item';
 import { itemActionTypes } from './actionTypes';
 
-export const getItems = (filteringData: FilteringData): ItemAction => ({
+export const getItems = (filteringData: FilteringData): GET_ITEMS => ({
     type: itemActionTypes.GET_ITEMS,
     payload: filteringData,
 });
 
-export const getItemsSuccess = (items: Item[]): ItemAction => ({
+export const getItemsSuccess = (items: Item[]): GET_ITEMS_SUCCESS => ({
     type: itemActionTypes.GET_ITEMS_SUCCESS,
     payload: items,
 });
 
-export const getItemsError = (): ItemAction => ({
+export const getItemsError = (): GET_ITEMS_ERROR => ({
     type: itemActionTypes.GET_ITEMS_ERROR,
 });
