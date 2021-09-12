fd593ceed4c14bc935eba0990a1f9cab6e7e9f06
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-09-12 04:14:17 +0300

    unique tags listing created

--- src/components/sidebar/TagFiltering.tsx
+++ src/components/sidebar/TagFiltering.tsx
@@ -11,15 +11,34 @@ const TagFiltering: React.FC<TagFilteringProps> = ({
     items,
     handleFilteringDataChange,
 }) => {
+    const allTagItems: any = [];
+
+    items.map((item) => {
+        return item.tags.map((tag) => {
+            return allTagItems.push(tag);
+        });
+    });
+
+    const uniqueTagItems = allTagItems.filter(
+        (item: any, index: any, arrRef: any) => arrRef.indexOf(item) === index,
+    );
+
     return (
         <div className="card">
             <div className="container">
                 <input className="filter-search" placeholder="Search tag" />
                 <div className="filter-overflow">
-                    {items.map((item) => (
-                        <label key={item.slug} className="sort-checkbox">
-                            All
-                            <input type="checkbox" />
+                    {uniqueTagItems.map((tag: any) => (
+                        <label key={tag} className="sort-checkbox">
+                            {tag}
+                            <input
+                                type="checkbox"
+                                name="tags"
+                                value={tag}
+                                onChange={(e) => {
+                                    handleFilteringDataChange(e);
+                                }}
+                            />
                             <span className="checkmark checkmark-square "></span>
                         </label>
                     ))}
