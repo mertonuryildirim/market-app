218f3245422aaaf402f3a2103bc6399f2a3ef71e
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-09-12 19:48:28 +0300

    comment out wrong code

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,21 +1,21 @@
 import { combineReducers } from 'redux';
-import { BasketState } from '../../types/basket';
+//import { BasketState } from '../../types/basket';
 import { CompanyState } from '../../types/company';
 import { ItemState } from '../../types/item';
-import basketReducer from './basketReducer';
+//import basketReducer from './basketReducer';
 import companyReducer from './companyReducer';
 import itemReducer from './itemReducer';
 
 export interface AppState {
     companies: CompanyState;
     items: ItemState;
-    basketItems: BasketState;
+    //basketItems: BasketState;
 }
 
 const rootReducer = combineReducers<AppState>({
     companies: companyReducer,
     items: itemReducer,
-    basketItems: basketReducer,
+    //basketItems: basketReducer,
 });
 
 export default rootReducer;
