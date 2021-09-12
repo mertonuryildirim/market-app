fbe3ea98f9d2938180c0233f352a08182367d40d
Mert Onur YILDIRIM <mert.yildirim@aartimuhendislik.com>
2021-09-12 14:28:59 +0300

    tag filtering functionality added

--- src/components/sidebar/TagFiltering.tsx
+++ src/components/sidebar/TagFiltering.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Item } from '../../types/item';
 import './Sidebar.css';
 
@@ -11,28 +11,41 @@ const TagFiltering: React.FC<TagFilteringProps> = ({
     items,
     handleFilteringDataChange,
 }) => {
-    const allTagItems: any = [];
+    const [tagItems, setTagItems] = useState([]);
 
-    items.map((item) => {
-        return item.tags.map((tag) => {
-            return allTagItems.push(tag);
+    useEffect(() => {
+        const allTagItems: any = [];
+
+        items.map((item) => {
+            return item.tags.map((tag) => {
+                return allTagItems.push(tag);
+            });
         });
-    });
 
-    const uniqueTagItems = allTagItems.filter(
-        (item: any, index: any, arrRef: any) => arrRef.indexOf(item) === index,
-    );
+        const uniqueTagItems = allTagItems.filter(
+            (item: any, index: any, arrRef: any) =>
+                arrRef.indexOf(item) === index,
+        );
 
-    console.log(uniqueTagItems);
+        setTagItems(uniqueTagItems);
+    }, [items]);
+
     return (
         <div className="card">
             <div className="container">
                 <input className="filter-search" placeholder="Search tag" />
                 <div className="filter-overflow">
-                    {uniqueTagItems.map((tag: any) => (
+                    {tagItems.map((tag: any) => (
                         <label key={tag} className="sort-checkbox">
                             {tag}
-                            <input type="checkbox" />
+                            <input
+                                type="checkbox"
+                                name="tags"
+                                value={tag}
+                                onClick={(e) => {
+                                    handleFilteringDataChange(e);
+                                }}
+                            />
                             <span className="checkmark checkmark-square "></span>
                         </label>
                     ))}
